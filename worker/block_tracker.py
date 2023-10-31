cd9a9eb084b67f36ab5d669262d2fc4a5d8e706b
hyeon <hyeon@planetariumhq.com>
2023-10-31 15:52:12 +0900

    Create block subscriber
    - Subscribes network and get Tx.s
    - Fetch staking data to calculate Sweep count
    - Send collected data to SQS block by block

---
+++ worker/block_tracker.py
@@ -0,0 +1,137 @@
+# This module must persist forever.
+# Run this module on server, not on lambda
+
+import json
+import logging
+import os
+import random
+import re
+from collections import defaultdict
+from threading import Thread
+
+import boto3
+import requests
+from gql import Client, gql
+from gql.transport.websockets import WebsocketsTransport
+
+from consts import HOST_LIST
+from worker.schemas.action import ActionJson
+
+
+class StakeAPCoef:
+    def __init__(self, gql_url):
+        self.gql_url = gql_url
+        # StakeActionPointCoefficientSheet Address: 0x4ce2d0Bc945c0E38Ae6c31B0dEe7030951eF1cD1
+        resp = requests.post(self.gql_url,
+                             json={"query": '{state(address: "0x4ce2d0Bc945c0E38Ae6c31B0dEe7030951eF1cD1")}'})
+        state = resp.json()["data"]["state"]
+        raw = bytes.fromhex(state)
+        self.data = raw.decode().split(":")[1]
+        head, *body = [x.split(",") for x in self.data.split("\n")]
+        d = defaultdict(int)
+        for b in body:
+            d[int(b[-1])] = int(b[1])
+
+        self.crit = []
+        _min = 0
+        for coef, val in d.items():
+            self.crit.append([range(_min, val), coef])
+            _min = val
+        self.crit.append([range(_min, 2 ** 64), coef])
+
+    def get_ap_coef(self, val):
+        for rng, coef in self.crit:
+            if val in rng:
+                return coef
+
+
+def get_deposit(coef: StakeAPCoef, url: str, result: dict, addr: str):
+    query = '{{stateQuery {{stakeState(address: "{addr}") {{deposit}}}}}}'.format(addr=addr)
+    resp = requests.post(url, json={"query": query})
+    data = resp.json()["data"]["stateQuery"]["stakeState"]
+    if data is None:
+        stake_amount = 0
+    else:
+        stake_amount = float(data["deposit"])
+
+    result[addr] = coef.get_ap_coef(stake_amount)
+
+
+def handler():
+    stage = os.environ.get("STAGE", "development")
+    url = f"{random.choice(HOST_LIST[stage])}/graphql"
+    transport = WebsocketsTransport(url=url)
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+    coef = StakeAPCoef(url)
+
+    # FIXME: Change actionType to regex: (hack_and_slash.*)|(battle_arena.*)|(raid.*)
+    reg = r"(hack_and_slash.*)|(battle_arena.*)|(raid.*)"
+    query = gql("""
+    subscription {
+        tx (actionType: "hack_and_slash21") {
+            txResult {txStatus, blockIndex}
+            transaction {
+                signer 
+                actions {json}
+            }
+        }
+    }
+    """)
+
+    # Init
+    tip = None
+    thread_list = []
+    action_data = []
+    stake_data = defaultdict(float)
+    regex = re.compile(reg)
+    sqs = boto3.client("sqs", region_name=os.environ.get("REGION_NAME"))
+
+    # Subscribe Tx. Forever
+    for result in client.subscribe(query):
+        block_index = result["tx"]["txResult"]["blockIndex"]
+        if tip is None:
+            tip = block_index
+        elif block_index != tip:
+            for t in thread_list:
+                t.join()
+            logging.info(f"{len(action_data)} actions sent to queue for block {tip}")
+            logging.info(f"{len(stake_data)} deposits fetched.")
+            logging.debug(stake_data)
+
+            message = {
+                "block": tip,
+                "action_data": dict(action_data),
+                "stake": dict(stake_data),
+            }
+            resp = sqs.send_message(
+                QueueUrl=os.environ.get("SQS_URL"),
+                MessageBody=json.dumps(message),
+            )
+            logging.info(f"Message {resp['MessageId']} sent do SQS.")
+
+            # Clear
+            action_data = defaultdict(list)
+            thread_list = []
+            stake_data = defaultdict(float)
+            tip = block_index
+
+        # Save action data and get NCG stake amount for later
+        if result["tx"]["txResult"]["txStatus"] == "SUCCESS":
+            signer = result["tx"]["transaction"]["signer"]
+            logging.debug(f"Action from {signer}")
+            # FIXME: Call thread only when `"sweep" in action_json.type_id`
+            t = Thread(target=get_deposit, args=(coef, url, stake_data, signer))
+            thread_list.append(t)
+            t.start()
+            for action in result["tx"]["transaction"]["actions"]:
+                action_json = ActionJson(**(json.loads(action["json"].replace(r"\uFEFF", ""))["values"]))
+                if regex.match(action_json.type_id):
+                    action_data[action_json.type_id].append({
+                        "agent_addr": signer,
+                        "avatar_addr": action_json.avatar_addr,
+                        "count_base": action_json.count_base,
+                    })
+
+
+if __name__ == "__main__":
+    handler()
---
+++ worker/schemas/action.py
@@ -0,0 +1,66 @@
+from typing import Optional, List
+
+from pydantic.v1.dataclasses import dataclass
+
+AP_PER_STONE = 120
+
+
+@dataclass
+class ActionJson:
+    id: str
+    type_id: str
+
+    # hack_and_slash21
+    avatarAddress: Optional[str] = None
+    apStoneCount: Optional[int] = None
+    costumes: Optional[List[str]] = None
+    equipments: Optional[List[str]] = None
+    foods: Optional[List[str]] = None
+    r: Optional[List[List[int]]] = None
+    stageId: Optional[int] = None
+    totalPlayCount: Optional[int] = None
+    worldId: Optional[int] = None
+
+    # hack_and_slash_sweep9
+    # avatarAddress: Optional[str] = None
+    actionPoint: Optional[int] = None
+    # apStoneCount: Optional[int] = None
+    # costumes: Optional[List[str]] = None
+    # equipments: Optional[List[str]] = None
+    runeInfos: Optional[List[List[int]]] = None
+    # stageId: Optional[int] = None
+    # worldId: Optional[int] = None
+
+    # battle_arena13
+    chi: Optional[int] = None
+    cs: Optional[List[str]] = None
+    eaa: Optional[str] = None
+    es: Optional[List[str]] = None
+    maa: Optional[str] = None
+    rd: Optional[int] = None
+    ri: Optional[List[List[int]]] = None
+    tk: Optional[int] = None
+
+    # raid6
+    a: Optional[str] = None
+    c: Optional[List[str]] = None
+    e: Optional[List[str]] = None
+    f: Optional[List[str]] = None
+    p: Optional[bool] = None
+    # r: Optional[List[List[int]]] = None
+
+    @property
+    def avatar_addr(self) -> str:
+        return self.avatarAddress or self.maa or self.a
+
+    @property
+    def count_base(self) -> int:
+        if self.type_id.startswith("raid"):
+            return 1
+        elif self.type_id.startswith("battle_arena"):
+            return self.tk
+        elif "sweep" in self.type_id:
+            # !!! WARNING: This returns used AP, not play count !!!
+            return self.actionPoint + self.apStoneCount * AP_PER_STONE
+        else:  # hack_and_slash
+            return self.totalPlayCount  # This includes AP and AP Potion usage + Staking modification.
