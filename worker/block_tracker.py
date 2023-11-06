695b0047f498690a65a3519a3cbc0058e4040b87
hyeon <hyeon@planetariumhq.com>
2023-11-07 02:47:06 +0900

    [TEST] Subscribe both tip and actions

--- worker/block_tracker.py
+++ worker/block_tracker.py
@@ -1,12 +1,8 @@
-# This module must persist forever.
-# Run this module on server, not on lambda
-
 import json
 import logging
 import os
 import random
 import re
-import time
 from collections import defaultdict
 from threading import Thread
 
@@ -34,9 +30,41 @@ def get_deposit(coef: StakeAPCoef, url: str, result: dict, addr: str):
     result[addr] = coef.get_ap_coef(stake_amount)
 
 
-def handle(event, context):
-    stage = os.environ.get("STAGE", "development")
-    url = f"{random.choice(HOST_LIST[stage])}/graphql"
+def send_message(tip: int, action_data: defaultdict, stake_data: defaultdict):
+    sqs = boto3.client("sqs", region_name=os.environ.get("REGION_NAME"))
+    message = {
+        "block": tip,
+        "action_data": dict(action_data),
+        "stake": dict(stake_data),
+    }
+    resp = sqs.send_message(
+        QueueUrl=os.environ.get("SQS_URL"),
+        MessageBody=json.dumps(message),
+    )
+    logging.info(f"Message {resp['MessageId']} sent to SQS for block {tip}.")
+
+
+def subscribe_tip(url: str, thread_dict: defaultdict, stake_data: defaultdict, action_data: defaultdict):
+    query = gql(""" subscription { tipChanged { index } } """)
+    transport = WebsocketsTransport(url=url.replace("https", "wss"))
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+    for result in client.subscribe(query):
+        tip = result["tipChanged"]["index"] - 1
+        for t in thread_dict[tip]:
+            t.join()
+        logging.info(f"{len(action_data[tip])} actions sent to queue for block {tip}")
+        logging.info(f"{len(stake_data[tip])} deposits fetched.")
+        logging.debug(stake_data)
+
+        send_message(tip, action_data[tip], stake_data[tip])
+
+        # Clear finished
+        del thread_dict[tip]
+        del action_data[tip]
+        del stake_data[tip]
+
+
+def subscribe_action(url: str, thread_dict: defaultdict, stake_data: defaultdict, action_data: defaultdict):
     transport = WebsocketsTransport(url=url.replace("https", "wss"))
     client = Client(transport=transport, fetch_schema_from_transport=True)
     coef = StakeAPCoef(url)
@@ -54,67 +82,51 @@ def handle(event, context):
         }}
     }}
     """)
-
-    # Init
-    tip = None
-    thread_list = []
-    action_data = defaultdict(list)
-    stake_data = defaultdict(float)
     regex = re.compile(reg)
-    sqs = boto3.client("sqs", region_name=os.environ.get("REGION_NAME"))
 
-    # Subscribe Tx. Forever
-    start = time.time()
     for result in client.subscribe(query):
         block_index = result["tx"]["txResult"]["blockIndex"]
-        if tip is None:
-            tip = block_index
-        elif block_index != tip:
-            for t in thread_list:
-                t.join()
-            logging.info(f"{len(action_data)} actions sent to queue for block {tip}")
-            logging.info(f"{len(stake_data)} deposits fetched.")
-            logging.debug(stake_data)
-
-            message = {
-                "block": tip,
-                "action_data": dict(action_data),
-                "stake": dict(stake_data),
-            }
-            resp = sqs.send_message(
-                QueueUrl=os.environ.get("SQS_URL"),
-                MessageBody=json.dumps(message),
-            )
-            logging.info(f"Message {resp['MessageId']} sent do SQS.")
-
-            # Clear
-            action_data = defaultdict(list)
-            thread_list = []
-            stake_data = defaultdict(float)
-            tip = block_index
-
-            # Finish subscribe after time limit
-            if (time.time() - start) >= EXEC_LIMIT:
-                break
-
         # Save action data and get NCG stake amount for later
         if result["tx"]["txResult"]["txStatus"] == "SUCCESS":
             signer = result["tx"]["transaction"]["signer"]
             logging.debug(f"Action from {signer}")
             # FIXME: Call thread only when `"sweep" in action_json.type_id`
             t = Thread(target=get_deposit, args=(coef, url, stake_data, signer))
-            thread_list.append(t)
+            thread_dict[block_index].append(t)
             t.start()
             for action in result["tx"]["transaction"]["actions"]:
                 action_raw = json.loads(action["json"].replace(r"\uFEFF", ""))
                 type_id = action_raw["type_id"]
                 action_json = ActionJson(type_id=type_id, **(action_raw["values"]))
                 if regex.match(action_json.type_id):
-                    action_data[action_json.type_id].append({
+                    action_data[block_index][action_json.type_id].append({
                         "agent_addr": signer,
                         "avatar_addr": action_json.avatar_addr,
                         "count_base": action_json.count_base,
                     })
+
+
+def handle(event, context):
+    stage = os.environ.get("STAGE", "development")
+    url = f"{random.choice(HOST_LIST[stage])}/graphql"
+
+    # Init
+    thread_dict = defaultdict(list)
+    stake_data = defaultdict(lambda: defaultdict(float))
+    action_data = defaultdict(lambda: defaultdict(list))
+
+    # Subscribe Tx. Forever
+    tip_thread = Thread(target=subscribe_tip, args=(url, thread_dict, stake_data, action_data))
+    action_thread = Thread(target=subscribe_action, args=(url, thread_dict, stake_data, action_data))
+    tip_thread.start()
+    action_thread.start()
+    tip_thread.join(timeout=EXEC_LIMIT)
+    action_thread.join(timeout=EXEC_LIMIT)
+
+    for block in action_data.keys():
+        for t in thread_dict[block]:
+            t.join()
+        send_message(block, action_data[block], stake_data[block])
 
 
 if __name__ == "__main__":
