009ec6f15256ec017c4692ce67c57d21694c290a
hyeon <hyeon@planetariumhq.com>
2023-11-18 14:59:52 +0900

    Revive old schema data for backward compatibility

--- season_pass/api/season_pass.py
+++ season_pass/api/season_pass.py
@@ -7,7 +7,7 @@ from common.models.season_pass import Level
 from common.utils.season_pass import get_current_season
 from season_pass.dependencies import session
 from season_pass.exceptions import SeasonNotFoundError
-from season_pass.schemas.season_pass import LevelInfoSchema, SeasonPassSchema, ExpInfoSchema
+from season_pass.schemas.season_pass import LevelInfoSchema, SeasonPassSchema, ExpInfoSchema, NewSeasonPassSchema
 
 router = APIRouter(
     prefix="/season-pass",
@@ -16,6 +16,57 @@ router = APIRouter(
 
 
 @router.get("/current", response_model=SeasonPassSchema)
+def old_current_season(sess=Depends(session)):
+    curr_season = get_current_season(sess)
+    if not curr_season:
+        raise SeasonNotFoundError("No active season pass for today")
+
+    return SeasonPassSchema(
+        id=curr_season.id,
+        start_date=curr_season.start_date,
+        end_date=curr_season.end_date,
+        reward_list=[
+            {
+                "level": reward["level"],
+                "normal": {
+                    "item": [
+                        {
+                            "id": x["ticker"].split("_")[-1],
+                            "amount": x["amount"]
+                        }
+                        for x in reward["normal"] if x["ticker"].startswith("Item_")
+                    ],
+                    "currency": [
+                        {
+                            "ticker": x["ticker"].split("_")[-1],
+                            "amount": x["amount"]
+                        }
+                        for x in reward["normal"] if x["ticker"].startswith("FAV__")
+                    ]
+                },
+                "premium": {
+                    "item": [
+                        {
+                            "id": x["ticker"].split("_")[-1],
+                            "amount": x["amount"]
+                        }
+                        for x in reward["premium"] if x["ticker"].startswith("Item_")
+                    ],
+                    "currency": [
+                        {
+                            "ticker": x["ticker"].split("_")[-1],
+                            "amount": x["amount"]
+                        }
+                        for x in reward["premium"] if x["ticker"].startswith("FAV__")
+                    ]
+                }
+            }
+            for reward in curr_season.reward_list
+        ]
+    )
+
+
+@router.get("/current/new", response_model=NewSeasonPassSchema)
 def current_season(sess=Depends(session)):
     curr_season = get_current_season(sess)
     if not curr_season:
