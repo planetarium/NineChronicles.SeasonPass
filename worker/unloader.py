7b63c248776526210400779f3dd31195e361ef6d
hyeon <hyeon@planetariumhq.com>
2024-01-03 11:47:03 +0900

    Reuse nonce if nonce already has been assigned

--- worker/unloader.py
+++ worker/unloader.py
@@ -37,6 +37,7 @@ def handle(event, context):
         nonce_dict = {}
 
         for i, record in enumerate(message.Records):
+            use_nonce = False
             claim = claim_dict.get(record.body.get("uuid"))
             if not claim:
                 logger.error(f"Cannot find claim {record.body.get('uuid')}")
@@ -53,11 +54,13 @@ def handle(event, context):
             else:
                 nonce = nonce_dict[claim.planet_id]
 
-            claim.nonce = nonce
+            if not claim.nonce:
+                claim.nonce = nonce
+                use_nonce = True
             claim.tx_status = TxStatus.CREATED
             unsigned_tx = gql.create_action(
                 claim.planet_id,
-                "claim_items", pubkey=account.pubkey, nonce=nonce,
+                "claim_items", pubkey=account.pubkey, nonce=claim.nonce,
                 avatar_addr=claim.avatar_addr, claim_data=claim.reward_list,
                 memo=json.dumps({"season_pass": {"n": claim.normal_levels, "p": claim.premium_levels, "t": "claim"}}),
             )
@@ -66,8 +69,8 @@ def handle(event, context):
             claim.tx = signed_tx.hex()
             sess.add(claim)
             target_claim_list.append(claim)
-
-            nonce_dict[claim.planet_id] += 1
+            if use_nonce:
+                nonce_dict[claim.planet_id] += 1
         sess.commit()
 
         for claim in target_claim_list:
