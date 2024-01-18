b44d301c21773152134e5cab7a4d0195df88ee5d
hyeon <hyeon@planetariumhq.com>
2024-01-18 13:51:09 +0900

    Setup for preview network

--- worker/worker_cdk_stack.py
+++ worker/worker_cdk_stack.py
@@ -45,7 +45,7 @@ class WorkerStack(Stack):
             ami = _ec2.MachineImage.lookup(
                 name="mainnet_9c_season-pass_block_tracker-20231219",
             )
-        elif self.config.stage == "internal":
+        elif self.config.stage in ("internal", "preview"):
             instance_type = _ec2.InstanceType.of(_ec2.InstanceClass.BURSTABLE4_GRAVITON, _ec2.InstanceSize.SMALL)
             ami = _ec2.MachineImage.lookup(
                 name="internal-9c-season_pass-block_tracker-20231209",
