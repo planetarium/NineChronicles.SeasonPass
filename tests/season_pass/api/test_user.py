63c2a745ad83edc5c3d08aec1e2a71aa4e65ab4d
hyeon <hyeon@planetariumhq.com>
2024-04-15 23:19:03 +0900

    Test for prev season status

--- tests/season_pass/api/test_user.py
+++ tests/season_pass/api/test_user.py
@@ -13,3 +13,33 @@ from season_pass.schemas.user import UserSeasonPassSchema
 tc = TestClient(app)
 
 
+@pytest.mark.usefixtures("sess")
+def test_prev_season_status_success(sess):
+    now = datetime.now(tz=timezone.utc)
+    prev_season_data = SeasonPass(
+        id=1,
+        start_timestamp=now - timedelta(days=3),
+        end_timestamp=now - timedelta(days=1),
+    )
+    current_season_data = SeasonPass(
+        id=2,
+        start_timestamp=now - timedelta(days=1),
+        end_timestamp=now + timedelta(days=1)
+    )
+    user_data = UserSeasonPass(
+        planet_id=PlanetID.ODIN_INTERNAL, season_pass_id=1,
+        agent_addr=TEST_AGENT_ADDR, avatar_addr=TEST_AVATAR_ADDR,
+    )
+    with add_test_data(sess, prev_season_data, current_season_data, user_data) as test_data:
+        prev_season, current_season, test_user = test_data
+        resp = tc.get("/api/user/status",
+                      params={
+                          "season_id": 1,
+                          "avatar_addr": test_user.avatar_addr,
+                          "planet_id": test_user.planet_id.decode(),
+                      })
+        assert resp.status_code == 200
+        data = resp.json()
+        data["planet_id"] = data["planet_id"].encode()
+        result = UserSeasonPassSchema(**data)
+        assert result.claim_limit_timestamp == prev_season.end_timestamp + timedelta(days=7)
