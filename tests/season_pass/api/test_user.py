784a7a65b18c01f61ee4d701f4c86c5c1a24d095
hyeon <hyeon@planetariumhq.com>
2024-04-15 23:17:46 +0900

    Move new user test to model test

--- tests/season_pass/api/test_user.py
+++ tests/season_pass/api/test_user.py
@@ -1,29 +1,15 @@
 from datetime import timezone, datetime, timedelta
 
 import pytest
+from fastapi.testclient import TestClient
 
 from common.enums import PlanetID
 from common.models.season_pass import SeasonPass
 from common.models.user import UserSeasonPass
 from conftest import TEST_AGENT_ADDR, TEST_AVATAR_ADDR, add_test_data
+from season_pass.main import app
+from season_pass.schemas.user import UserSeasonPassSchema
+
+tc = TestClient(app)
 
 
-@pytest.mark.usefixtures("sess")
-def test_new_user(sess):
-    now = datetime.now(tz=timezone.utc)
-    season_data = SeasonPass(
-        id=1, instant_exp=100,
-        start_timestamp=now - timedelta(days=1),
-        end_timestamp=now + timedelta(days=1),
-    )
-    user_data = UserSeasonPass(
-        planet_id=PlanetID.ODIN_INTERNAL,
-        agent_addr=TEST_AGENT_ADDR,
-        avatar_addr=TEST_AVATAR_ADDR,
-        season_pass_id=1
-    )
-    with add_test_data(sess, season_data, user_data) as test_data:
-        test_season, test_user = test_data
-        assert test_user.agent_addr == TEST_AGENT_ADDR
-        assert test_user.avatar_addr == TEST_AVATAR_ADDR
-        assert test_user.season_pass_id == 1
