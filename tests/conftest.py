8e63fd09dfbe79eeb330b08e5f6ace8623a75d80
hyeon <hyeon@planetariumhq.com>
2024-04-15 22:37:17 +0900

    Update user test to use contextmanager

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,12 @@
-import datetime
-import json
 import os
+from contextlib import contextmanager
+from typing import List
 
 import alembic.command
 import pytest
 from alembic.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-from common.models.season_pass import Level, SeasonPass
-from common.models.user import UserSeasonPass
-from common.enums import PlanetID
 
 TEST_AGENT_ADDR = "0x49D5FcEB955800B2c532D6319E803c7D80f817Af"
 TEST_AVATAR_ADDR = "0xC8cA85aE399de5c4dcAd39e8A13cfA7cBcEff066"
@@ -47,36 +43,36 @@ def session(engine):
         sess.close()
 
 
-@pytest.fixture(scope="session", autouse=True)
-@pytest.mark.usefixtures("session")
-def set_test_data(session):
-    with open("tests/data/level.json", "r") as f:
-        level_data = json.loads(f.read())
-    for lvl in level_data:
-        session.add(Level(level=lvl["level"], exp=lvl["exp"]))
-
-    with open("tests/data/reward.json", "r") as f:
-        reward = json.loads(f.read())
-
-    start_timestamp = datetime.datetime.now(tz=datetime.timezone.utc).replace(day=1)
-    session.add(
-        SeasonPass(id=1, start_timestamp=start_timestamp, end_timestamp=start_timestamp + datetime.timedelta(days=28),
-                   reward_list=reward, instant_exp=10000)
-    )
-
-    session.commit()
-
-
-@pytest.fixture(scope="session")
-@pytest.mark.usefixtures("session")
-def new_user(session):
-    nu = UserSeasonPass(agent_addr=TEST_AGENT_ADDR, avatar_addr=TEST_AVATAR_ADDR, season_pass_id=1,
-                        planet_id=PlanetID.ODIN_INTERNAL)
-    session.add(nu)
-    session.commit()
-    yield nu
-    session.delete(nu)
-    session.commit()
+# @pytest.fixture(scope="session", autouse=True)
+# @pytest.mark.usefixtures("session")
+# def set_test_data(session):
+#     with open("tests/data/level.json", "r") as f:
+#         level_data = json.loads(f.read())
+#     for lvl in level_data:
+#         session.add(Level(level=lvl["level"], exp=lvl["exp"]))
+#
+#     with open("tests/data/reward.json", "r") as f:
+#         reward = json.loads(f.read())
+#
+#     start_timestamp = datetime.datetime.now(tz=datetime.timezone.utc).replace(day=1)
+#     session.add(
+#         SeasonPass(id=1, start_timestamp=start_timestamp, end_timestamp=start_timestamp + datetime.timedelta(days=28),
+#                    reward_list=reward, instant_exp=10000)
+#     )
+#
+#     session.commit()
+#
+#
+# @pytest.fixture(scope="session")
+# @pytest.mark.usefixtures("session")
+# def new_user(session):
+#     nu = UserSeasonPass(agent_addr=TEST_AGENT_ADDR, avatar_addr=TEST_AVATAR_ADDR, season_pass_id=1,
+#                         planet_id=PlanetID.ODIN_INTERNAL)
+#     session.add(nu)
+#     session.commit()
+#     yield nu
+#     session.delete(nu)
+#     session.commit()
 
 
 @pytest.fixture(scope="function")
@@ -87,3 +83,20 @@ def sess(engine):
         yield s
     finally:
         s.close()
+
+
+@contextmanager
+def add_test_data(sess, *test_data: List) -> List:
+    updated_data = []
+    try:
+        for data in test_data:
+            sess.add(data)
+        sess.commit()
+        for data in test_data:
+            sess.refresh(data)
+            updated_data.append(data)
+        yield updated_data
+    finally:
+        for data in updated_data:
+            sess.delete(data)
+        sess.commit()
