ca7265a7e0d1e74bc3b0949d8f33b26b008e92ee
hyeon <hyeon@planetariumhq.com>
2024-10-28 22:23:57 +0900

    Add season_index to distinguish season index group by season type

--- common/models/season_pass.py
+++ common/models/season_pass.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, JSON, ForeignKey, DateTime, Enum
+from sqlalchemy import Column, Integer, JSON, ForeignKey, DateTime, Enum, Index
 from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.orm import relationship
 
@@ -11,12 +11,17 @@ class SeasonPass(TimeStampMixin, Base):
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     pass_type = Column(Enum(PassType), nullable=False, server_default=PassType.COURAGE_PASS.name,
                        doc="Type of this pass")
+    season_index = Column(Integer, nullable=False, doc="Season index of this type of pass.")
     start_timestamp = Column(DateTime(timezone=True), nullable=True, doc="Start datetime of this season. Inclusive.")
     end_timestamp = Column(DateTime(timezone=True), nullable=True, doc="End datetime of this season. Inclusive.")
     reward_list = Column(JSON, nullable=False, default=[])
     instant_exp = Column(Integer, nullable=False, doc="Instant reward exp for premium plus user")
 
     exp_list = relationship("Exp")
+
+    __table_args__ = (
+        Index("ix_pass_type_index_unique", "pass_type", "season_index", unique=True),
+    )
 
     @property
     def exp_dict(self):
