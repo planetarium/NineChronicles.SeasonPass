52f4b87d099349b96de544594f766db1250f66a5
hyeon <hyeon@planetariumhq.com>
2024-11-01 17:48:26 +0900

    More tracker update
    - Move block data fetching logic to utils
    - One tracker tracks one pass type

--- worker/courage_tracker.py
+++ worker/courage_tracker.py
@@ -3,7 +3,6 @@ import json
 import os
 from collections import defaultdict
 
-import boto3
 import requests
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker, scoped_session
@@ -16,7 +15,7 @@ from common.utils.season_pass import create_jwt_token
 from schemas.action import ActionJson
 from utils.stake import StakeAPCoef
 from worker.utils.aws import send_sqs_message
-from worker.utils.gql import get_block_tip
+from worker.utils.gql import get_block_tip, fetch_block_data
 
 GQL_URL = os.environ.get("GQL_URL")
 CURRENT_PLANET = PlanetID(os.environ.get("PLANET_ID").encode())
@@ -24,12 +23,6 @@ DB_URI = os.environ.get("DB_URI")
 db_password = fetch_secrets(os.environ.get("REGION_NAME", "us-east-2"), os.environ.get("SECRET_ARN"))["password"]
 DB_URI = DB_URI.replace("[DB_PASSWORD]", db_password)
 SQS_URL = os.environ.get("SQS_URL")
-
-TARGET_ACTION_DICT = {
-    PassType.COURAGE_PASS: "(hack_and_slash.*)|(battle_arena.*)|(raid.*)|(event_dungeon_battle.*)",
-    PassType.ADVENTURE_BOSS_PASS: "(wanted.*)|(explore_adventure_boss.*)|(sweep_adventure_boss.*)",
-    PassType.WORLD_CLEAR_PASS: "(hack_and_slask.*)"
-}
 
 engine = create_engine(DB_URI)
 
@@ -51,30 +44,7 @@ def get_deposit(coef: StakeAPCoef, addr: str) -> float:
 
 
 def process_block(block_index: int, pass_type: PassType):
-    # Fetch Tx. and actions
-    nct_query = f"""{{ transaction {{ ncTransactions (
-        startingBlockIndex: {block_index},
-        limit: 1,
-        actionType: "{TARGET_ACTION_DICT[pass_type]}"
-    ) {{ id signer actions {{ json }} }}
-    }} }}"""
-    resp = requests.post(
-        GQL_URL,
-        json={"query": nct_query},
-        headers={"Authorization": f"Bearer {create_jwt_token(os.environ.get('HEADLESS_GQL_JWT_SECRET'))}"}
-    )
-    tx_data = resp.json()["data"]["transaction"]["ncTransactions"]
-
-    tx_id_list = [x["id"] for x in tx_data]
-
-    # Fetch Tx. results
-    tx_result_query = f"""{{ transaction {{ transactionResults (txIds: {json.dumps(tx_id_list)}) {{ txStatus }} }} }}"""
-    resp = requests.post(
-        GQL_URL,
-        json={"query": tx_result_query},
-        headers={"Authorization": f"Bearer {create_jwt_token(os.environ.get('HEADLESS_GQL_JWT_SECRET'))}"}
-    )
-    tx_result_list = [x["txStatus"] for x in resp.json()["data"]["transaction"]["transactionResults"]]
+    tx_data, tx_result_list = fetch_block_data(block_index, pass_type)
 
     action_data = defaultdict(list)
     agent_list = set()
@@ -107,7 +77,7 @@ def process_block(block_index: int, pass_type: PassType):
 def main():
     sess = scoped_session(sessionmaker(bind=engine))
     # Get missing blocks
-    expected_all = set(range(int(os.environ.get("START_BLOCK_INDEX")), get_block_tip(GQL_URL) + 1))
+    expected_all = set(range(int(os.environ.get("START_BLOCK_INDEX")), get_block_tip() + 1))
     all_blocks = set(sess.scalars(select(Block.index).where(Block.planet_id == CURRENT_PLANET)).fetchall())
     missing_blocks = expected_all - all_blocks
 
@@ -115,8 +85,6 @@ def main():
     with concurrent.futures.ThreadPoolExecutor() as executor:
         for index in missing_blocks:
             block_dict[executor.submit(process_block, index, PassType.COURAGE_PASS)] = (index, PassType.COURAGE_PASS)
-            block_dict[executor.submit(process_block, index, PassType.ADVENTURE_BOSS_PASS)] = (index, PassType.ADVENTURE_BOSS_PASS)
-            block_dict[executor.submit(process_block, index, PassType.WORLD_CLEAR_PASS)] = (index, PassType.WORLD_CLEAR_PASS)
 
         for future in concurrent.futures.as_completed(block_dict):
             index = block_dict[future]
--- worker/utils/gql.py
+++ worker/utils/gql.py
@@ -1,14 +1,52 @@
+import json
 import os
 
 import requests
 
+from common.enums import PassType
 from common.utils.season_pass import create_jwt_token
 
 
-def get_block_tip(url: str):
+GQL_URL = os.environ.get("GQL_URL")
+TARGET_ACTION_DICT = {
+    PassType.COURAGE_PASS: "(hack_and_slash.*)|(battle_arena.*)|(raid.*)|(event_dungeon_battle.*)",
+    PassType.ADVENTURE_BOSS_PASS: "(wanted.*)|(explore_adventure_boss.*)|(sweep_adventure_boss.*)",
+    PassType.WORLD_CLEAR_PASS: "(hack_and_slask.*)"
+}
+
+
+def get_block_tip():
     resp = requests.post(
-        url,
+        GQL_URL,
         json={"query": "{ nodeStatus { tip { index } } }"},
         headers={"Authorization": f"Bearer {create_jwt_token(os.environ.get('HEADLESS_GQL_JWT_SECRET'))}"}
     )
     return resp.json()["data"]["nodeStatus"]["tip"]["index"]
+
+
+def fetch_block_data(block_index: int, pass_type: PassType):
+    # Fetch Tx. and actions
+    nct_query = f"""{{ transaction {{ ncTransactions (
+        startingBlockIndex: {block_index},
+        limit: 1,
+        actionType: "{TARGET_ACTION_DICT[pass_type]}"
+    ) {{ id signer actions {{ json }} }}
+    }} }}"""
+    resp = requests.post(
+        GQL_URL,
+        json={"query": nct_query},
+        headers={"Authorization": f"Bearer {create_jwt_token(os.environ.get('HEADLESS_GQL_JWT_SECRET'))}"}
+    )
+    tx_data = resp.json()["data"]["transaction"]["ncTransactions"]
+
+    tx_id_list = [x["id"] for x in tx_data]
+
+    # Fetch Tx. results
+    tx_result_query = f"""{{ transaction {{ transactionResults (txIds: {json.dumps(tx_id_list)}) {{ txStatus }} }} }}"""
+    resp = requests.post(
+        GQL_URL,
+        json={"query": tx_result_query},
+        headers={"Authorization": f"Bearer {create_jwt_token(os.environ.get('HEADLESS_GQL_JWT_SECRET'))}"}
+    )
+    tx_result_list = [x["txStatus"] for x in resp.json()["data"]["transaction"]["transactionResults"]]
+    return tx_data, tx_result_list
