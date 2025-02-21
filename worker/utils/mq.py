17a4a6673ee42ef06436ede7e8cff9321d0dbde5
Yang Chun Ung <qooraven@gmail.com>
2025-02-21 11:27:33 +0900

    mq host configurable

--- worker/utils/mq.py
+++ worker/utils/mq.py
@@ -1,10 +1,13 @@
 import json
 import logging
+import os
 from collections import defaultdict
 
 from pika import BlockingConnection, ConnectionParameters
 
 from common.enums import PlanetID
+
+queue_host = os.environ["MQ_HOST"]
 
 
 def send_message(planet: PlanetID, queue: str, index: int, action_data: defaultdict):
@@ -13,9 +16,14 @@ def send_message(planet: PlanetID, queue: str, index: int, action_data: defaultd
         "block": index,
         "action_data": dict(action_data),
     }
-    connection = BlockingConnection(ConnectionParameters("localhost"))
+    connection = get_connection()
     channel = connection.channel()
     channel.queue_declare(queue=queue)
     channel.basic_publish(exchange="", routing_key=queue, body=json.dumps(message))
     logging.info(f"Message {message} sent to SQS for block {index}.")
     connection.close()
+
+
+def get_connection() -> BlockingConnection:
+    connection = BlockingConnection(ConnectionParameters(queue_host))
+    return connection
