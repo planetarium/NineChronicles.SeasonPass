9741c9b70448cdd88fec6f96573f4d405d0076c2
Atralupus <me@atralupus.com>
2025-03-18 17:18:13 +0900

    fix region name

--- apps/worker/app/consumers/claim_consumer.py
+++ apps/worker/app/consumers/claim_consumer.py
@@ -27,7 +27,9 @@ def consume_claim_message(message: ClaimMessage):
     """
 
     sess = scoped_session(sessionmaker(bind=engine))
-    account = Account(fetch_kms_key_id(config.stage, config.region_name))
+    account = Account(
+        fetch_kms_key_id(config.stage, config.region_name), config.region_name
+    )
     gql = GQLClient(config.converted_gql_url_map, config.headless_jwt_secret)
 
     try:
